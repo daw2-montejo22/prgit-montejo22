af06b82676b44355945e60fa75fe7887f31de27b
daw2-montejo22 <montejoalvarezjosecarlos@fpllefia.com>
2021-11-18 16:48:41 +0100

    "prgit-montejo22v1"

---
+++ mitja.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	float suma=0;
+	float mitja;
+	float nou_valor;
+	int numero_repeticions=0;
+	char consulta='s';
+	
+	while(consulta=='s') {
+		printf("montejo22");
+		scanf("%f",&nou_valor);
+		suma=suma+nou_valor;
+		numero_repeticions=numero_repeticions+1;
+		printf("montejo22");
+		scanf(" %c",&consulta); 
+	}
+	mitja=suma/numero_repeticions;
+	printf("montejo22",mitja);	
+	return 0;
+}
